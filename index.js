71d8f41947b2d88a15cdfb82e979af90dc4fa849
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-01-13 17:00:11 +0200

    add prompt and change logic

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const number = prompt('Enter random number');
-let array = Array(number.length);
+const number = +prompt('The length of the array');
+let array = [];
 
-for (let i=0; i<number.length; i++) {
-    array[i] = number[i];
+for (let i = 0; i < number; i++) {
+    array[i] = prompt(`Enter ${number} values for your array (one at a time)`);
 }
 console.log(array);
 
 function compareNumbers(a, b) {
     return a - b;
 }
+
 console.log(array.sort(compareNumbers)) ;
 
 const spliced = array.splice(1, 3);
